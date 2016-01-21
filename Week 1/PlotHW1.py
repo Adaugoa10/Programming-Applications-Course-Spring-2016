5656d90630d272569b023cee96b7022766edb93a
Adaugoa10 <christineanyamele@yahoo.com>
2016-01-21 00:17:26 -0500

    Update PlotHW1.py

--- Week 1/PlotHW1.py
+++ Week 1/PlotHW1.py
@@ -13,8 +13,26 @@
 
 #add legend
 
+import matplotlib.pyplot as plt
+import numpy as np
+plt.rcdefaults()
+from numpy import sin, cos, pi
+
+a = np.linspace(0,6*pi,100)
+ysin_=sin(a)
+ycos_=cos(a)
+
+plt.gca().set_color_cycle(['blue','red'])
+plt.plot(a,ysin_)
+plt.plot(a,ycos_)
+plt.ylabel("y")
+plt.xlabel("x")
+plt.legend(('sin(x)','cos(x)'))
+plt.title('Plot of sinx and cosx for 0 to 6pi')
+
+plt.show()
 
 #leave comment here with your information
-#Name:
-#Date:
+#Name:Adaugo Anyamele
+#Date:1/21/2016
 
